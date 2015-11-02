8af11a55ae2d39eca203652b153069e10a70a702
Junya Wako <junwako@gmail.com>
2015-11-02 17:29:08 +0900

    Add log

--- recipes/write_config.rb
+++ recipes/write_config.rb
@@ -1,12 +1,13 @@
 # Set up app's custom configuration in the environment.
 # See https://forums.aws.amazon.com/thread.jspa?threadID=118107
+Chef::Log.info 'Running recipes/write_config.rb'
 
 node[:deploy].each do |application, deploy|
-  
+
   custom_env_template do
     application application
     deploy deploy
     env node[:custom_env][application]
   end
-  
+
 end
